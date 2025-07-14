ed1ed258998c23bb2251d2c6a6081d28cc990496
Navraj Awasthi <awasthintr07@gmail.com>
2025-07-14 20:52:57 +0545

    server added

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -5,7 +5,7 @@ import { CourseContextProvider } from "./context/CourseContext.jsx";
 import { UserContextProvider } from "./context/UserContext.jsx";
 import "./index.css";
 
-export const server = "http://localhost:5000";
+export const server = "https://e-learning-platform-5i59.onrender.com";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
