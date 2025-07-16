eb59fd0a4bb02c715a25638d2f00794369627ee3
Navraj Awasthi <awasthintr07@gmail.com>
2025-07-17 00:16:38 +0545

    admin error corrected

--- Frontend/src/admin/Courses/AdminCourses.jsx
+++ Frontend/src/admin/Courses/AdminCourses.jsx
@@ -1,7 +1,7 @@
 import PropTypes from "prop-types";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import CourseCard from "../../components/coursecard/CourseCard";
+import CourseCard from "../../components/courseCard/CourseCard";
 import { CourseData } from "../../context/CourseContext";
 import Layout from "../Utils/Layout";
 
